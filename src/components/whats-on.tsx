6ecfb9a986a30acb6be63b78a89f1fe10babc203
Heath Maes <contact@seammedia.com.au>
2026-08-13 15:23:51 +1000

    Move Wine Club poster to bottom

--- src/components/whats-on.tsx
+++ src/components/whats-on.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const posters = [
   { name: "Aperitivo Time", image: "/images/whatson-aperitivo.jpg", alt: "Aperitivo Time - Wed to Fri 5-7pm, $4 oysters, margaritas, spritz and wine" },
-  { name: "Wine Club", image: "/images/whatson-wine-club.jpg", alt: "Elvina Wine Club - monthly Thursday dinners with remarkable winemakers" },
   { name: "Bottomless", image: "/images/whatson-bottomless-pink.jpg", alt: "Bottomless lunch and dinner - $99 including a signature banquet and two-hour drinks package" },
   { name: "Wagyu Wednesday", image: "/images/whatson-wagyu.jpg", alt: "Wagyu Wednesday - $33 Tajima MB4+ wagyu rump with fries and eschallot butter" },
   { name: "Vino Vinyl Fridays", image: "/images/whatson-vinyl.jpg", alt: "Vino Vinyl Fridays at Elvina - drinks, tunes and food with music by Luke Tommy G" },
+  { name: "Wine Club", image: "/images/whatson-wine-club.jpg", alt: "Elvina Wine Club - monthly Thursday dinners with remarkable winemakers" },
 ];
 
 export function WhatsOn({ eagerFirstPoster = false }: { eagerFirstPoster?: boolean }) {
